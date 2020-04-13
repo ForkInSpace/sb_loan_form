7aa6f02cdfc5473cd8ffd514dd16278f51aa1a0b
Zazasiv <ts.zaza@gmail.com>
2020-04-13 23:51:11 +0300

    save co-borrower data with sessionStorage

--- src/components/more-info/more-info.controller.js
+++ src/components/more-info/more-info.controller.js
@@ -1,12 +1,35 @@
 export default class MoreInfoController {
-	constructor($log) {
+	constructor($log, $window) {
 		'ngInject';
 
 		this.$log = $log;
+		this.$window = $window;
 	}
 
 	$onInit = () => {
 		this.heading = 'Additional info';
-		this.$log.info('Activated more info View.');
+		this.info = {
+			name: '',
+			income: 350,
+			debt: 0
+		};
+		this.getFromSession(this.info);
 	};
+
+	saveToSession(obj){
+		return Object.keys(obj).forEach((item) => {
+			this.$window.sessionStorage.setItem(item, this.info[item]);
+		})
+	}
+
+	getFromSession(obj){
+		return Object.keys(obj).forEach((item) => {
+			this.info[item] = this.$window.sessionStorage.getItem(item);
+		})
+	}
+
+	storeTheValue() {
+		this.saveToSession(this.info);
+	}
+
 }
